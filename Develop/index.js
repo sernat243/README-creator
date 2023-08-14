0baec14353898ef4cb784210a60b29ab3f0bba89
Gabriel <sernat243@gmail.com>
2023-08-14 16:29:21 -0600

    created function to write readme file and to initialize app

--- Develop/index.js
+++ Develop/index.js
@@ -3,6 +3,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -39,11 +41,21 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("README file created");
+        }
+    });
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const markdown = generateMarkdown(answers);
+    });
+}
 
 // Function call to initialize app
 init();
