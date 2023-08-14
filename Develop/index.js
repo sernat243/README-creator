e860ee56ebbd8d22b5e33f352e4ddde2d628174a
Gabriel <sernat243@gmail.com>
2023-08-14 16:49:11 -0600

    called writetofile function inside init

--- Develop/index.js
+++ Develop/index.js
@@ -54,6 +54,7 @@ function writeToFile(fileName, data) {
 function init() {
     inquirer.prompt(questions).then((answers) => {
         const markdown = generateMarkdown(answers);
+        writeToFile('README.md', markdown);
     });
 }
 
