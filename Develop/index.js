9b320f4d2af6f5a578229de5ed6e85a3bd9776ec
Gabriel <sernat243@gmail.com>
2023-08-14 16:23:35 -0600

    installed inquirer and added prompt questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,10 +1,46 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'Title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Provide project description'
+    },
+    {
+        type:'input',
+        name:'Installation',
+        message:'Provide installation instructions',
+    },
+    {
+        type:'input',
+        name:'Usage',
+        message:'Provide usage instructions',
+    },
+    {
+        type:'input',
+        name:'Contributing',
+        message:'Provide contribution guidelines',
+    },
+    {
+        type:'input',
+        name:'Tests',
+        message:'Provide',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
